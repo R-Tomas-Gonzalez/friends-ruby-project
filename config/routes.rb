7c03c8191826a7b884c17bb65afefc6f86028c91
Tomas Gonzalez <r.tomas.gonzalez01@gmail.com>
2024-01-09 15:54:46 -0600

    added devise to ruby file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :friends
   # get 'home/index' <- automatically created this route when we generated the page
   root 'home#index' # <- manually created this to set home page to the route I wanted. default root page
