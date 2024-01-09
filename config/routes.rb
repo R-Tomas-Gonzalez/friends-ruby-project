f4eff42127b418ecb4c1c8e67ff7fb2b50c292d8
Tomas Gonzalez <r.tomas.gonzalez01@gmail.com>
2024-01-09 17:36:12 -0600

    styling adjustments

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :friends
   # get 'home/index' <- automatically created this route when we generated the page
-  root 'home#index' # <- manually created this to set home page to the route I wanted. default root page
+  #root 'home#index' # <- manually created this to set home page to the route I wanted. default root page
+  root 'friends#index'
   get 'home/about' 
 
   get "up" => "rails/health#show", as: :rails_health_check
