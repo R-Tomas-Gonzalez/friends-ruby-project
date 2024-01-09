dd2527be7029bb38e909052b718d39159d880deb
Tomas Gonzalez <r.tomas.gonzalez01@gmail.com>
2024-01-09 15:08:22 -0600

    initializing project. Styled, and added friends mvc

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :friends
+  # get 'home/index' <- automatically created this route when we generated the page
+  root 'home#index' # <- manually created this to set home page to the route I wanted. default root page
+  get 'home/about' 
+
+  get "up" => "rails/health#show", as: :rails_health_check
+end
